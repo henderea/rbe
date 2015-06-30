4effd95a9f55eb136876424804adf4ff0e9244ed
Eric Henderson <henderea@gmail.com>
2015-06-30 12:20:32 -0400

    do some refactoring

--- lib/rbe/cli/commands/alias.rb
+++ lib/rbe/cli/commands/alias.rb
@@ -3,7 +3,7 @@ require 'rbe/data/data_store'
 require 'everyday_thor_util/builder'
 include EverydayThorUtil::Builder
 
-root_command[:alias] = command(short_desc: 'alias SUBCOMMAND ARGS...', desc: 'configure command bash aliases')
+root_command[:alias] = command(short_desc: 'alias SUBCOMMAND ARGS...', desc: 'configure command bash alias functions')
 
 root_command[:alias][:add] = command(short_desc: 'add cmd_id...', desc: 'register an alias') { |*cmd_ids|
   cmd_ids.each { |cmd_id|
@@ -24,63 +24,7 @@ root_command[:alias][:list] = command(aliases: %w(ls), short_desc: 'list', desc:
 }
 
 root_command[:alias][:update] = command(aliases: %w(up), short_desc: 'update [prefix="rvm reload 2>&- >&-"]', desc: 'update the bash files for the aliases that are registered with an optional prefix command') { |prefix = 'rvm reload 2>&- >&-'|
-  IO.write(File.expand_path('~/rbe_reload.sh'),
-<<EOS
-function rbe_reload {
-    #{prefix}
-    rbe alias update
-    source ~/rbe_reload.sh
-    source ~/rbe_aliases.sh
-}
-EOS
-)
-  list = Rbe::Data::DataStore.aliases.list
-  str = <<EOS
-function rvsu {
-    #{prefix}
-    rbe s --rvm-sudo $@
-}
-function rsu {
-    #{prefix}
-    rbe s $@
-}
-function rce {
-    #{prefix}
-    rbe c e $@
-}
-function rta {
-    #{prefix}
-    rbe test-auth $@
-}
-function raa {
-    #{prefix}
-    rbe alias add $@
-    rbe_reload
-}
-EOS
-  list.each { |li|
-    str << <<EOS
-function #{li.to_s} {
-    rce #{li.to_s} $@
-}
-EOS
-  }
-  IO.write(File.expand_path('~/rbe_aliases.sh'), str)
-
-  load_line = '[[ -s "$HOME/rbe_reload.sh" ]] && source "$HOME/rbe_reload.sh" && source "$HOME/rbe_aliases.sh" # Load rbe_reload.sh'
-  lines = IO.readlines(File.expand_path('~/.bashrc')).map(&:chomp)
-  ind = -1
-  lines.each_with_index { |l, i|
-    if l.to_s.include? 'rbe_reload.sh'
-      ind = i
-      break
-    end
-  }
-  if ind >= 0
-    lines[ind] = load_line
-  else
-    lines << load_line
-  end
-
-  IO.write(File.expand_path('~/.bashrc'), lines.join("\n"))
+  update_rbe_reload(prefix)
+  update_rbe_aliases(prefix)
+  update_bashrc
 }
